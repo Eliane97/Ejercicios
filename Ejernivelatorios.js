f395cf8c138a4d5c6c731842d8614af3540597fc
Eliane <101228113+Eliane97@users.noreply.github.com>
2022-03-29 22:01:22 -0300

    Add files via upload

---
+++ Ejernivelatorios.js
@@ -0,0 +1,49 @@
+/*Ejercicio 1
+declarar una variable que se llame nombre
+el dato que le pasamos a la variable se lo pedimos al usuario en un prompt
+mostramos un alerta con la cantidad de caracteres del nombre que ingreso el usuario/a */
+// Ejercicio hecho
+/* let nombre = prompt("Ingrese su nombre");
+alert(nombre.length); */
+
+
+// Ejercicio 2
+/* pido un nombre al usuario/a
+pido al usuario/a que ingrese un numero
+hola "nombre de usuaria/o te pedimos que ingreses un numero"
+alerta con "nombreusuaria/o" tiene (numero de letras) caracteres; 
+le sumamos el numero que ingresaste el resultado es (numero de letras + numero ingresado) */
+
+/* let nombre2 = prompt("Ingrese su nombre");
+let numero = Number( prompt (`Hola ${nombre2} ingresa un número`));
+resultado = nombre2.length + numero;
+alert(`${nombre2} su nombre tiene ${nombre2.length} carácteres, estos caracteres sumado al número que ingresó da como resultado ${resultado}`);
+ */
+
+/* Ejercicio 3
+ Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga «Hello Arbi */
+
+ // alert("Hello Arbi");
+
+/* Ejercicio 4
+//Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello Arbi» (document.write) */
+
+//document.write("Hello Arbi");
+
+/* Ejercicio 5
+Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5 */
+
+//alert(3+5);
+
+/* Ejercicio 6
+Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola (nombre)»
+ */
+
+//let nombreusuario = prompt("Ingrese su nombre");
+// alert(`Hola ${nombreusuario}`);
+
+/* Ejercicio 7
+Escribe un programa de dos lineas que pida el nombre al usuario con un prompt y escriba un texto que diga «Hola (nombre) tu nombre tiene (cantidad) letras */
+
+// let nombreusuario2 = prompt("Ingrese su nombre");
+// alert(`Hola ${nombreusuario2} tu nombre tiene ${nombreusuario2.length} letras`);
